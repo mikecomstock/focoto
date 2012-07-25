20c7d11a56a560c932eef687cd233f5b14826d0a
Mike Munroe <mike@mikemunroe.com>
2012-07-24 21:38:54 -0400

    Basic rails project skeleton
    utilize bootstrap for styles
    add two static pages: home & photos
    basic layout with header and footer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Focoto::Application.config.secret_token = 'eece2b06205ef5ed524db335567e3a01996bb5484421faddaabe62141619d777137a23564cc7c8c9d4b629154d15ae5f050652b1c6ff70713887913c7d6af6bd'
