8d3d760bbd93d4594681ba5277dc8e139d069a05
Mike Comstock <mikecomstock@gmail.com>
2012-07-28 20:57:41 +0000

    support urls like /boards/23/posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,12 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    
+    if params[:board_id]
+      @posts = Board.find(params[:board_id]).posts
+    else
+      @posts = Post.all
+    end
 
     respond_to do |format|
       format.html # index.html.erb
